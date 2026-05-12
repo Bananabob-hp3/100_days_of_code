190a55203ca7676ff30b2d357136d2d8f70b212c
Bananabob-hp3 <pantharahit97@gmail.com>
2026-05-12 21:42:00 +0530

    Add day-25 US states game

---
+++ day-25/us_states_main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+import csv
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+
+
+screen.addshape(image)
+turtle.shape(image)
+t = turtle.Turtle()
+
+
+
+game_is_on = True
+data = pandas.read_csv("50_states.csv")
+correct_guess = []
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="Whats another states's name").title()
+    answer = data[data.state == answer_state]
+    if answer_state in data.state.values:
+        correct_guess.append(answer_state)
+        x = answer.x.values[0]
+        y = answer.y.values[0]
+        t.penup()
+        t.hideturtle()
+        t.goto(x, y)
+        t.write(answer_state)
+ 
+    if len(correct_guess) == 50:
+        game_is_on = False
+
+
+
+
+
+
+turtle.mainloop()
