ec97923cc9d35ccb8994126cba32dd2f241323d2
Bananabob-hp3 <pantharahit97@gmail.com>
2026-05-24 21:05:03 +0530

    ISS header locator project - needs to add email

---
+++ day-33/ISS Overhead Notifier Position/main.py
@@ -0,0 +1,80 @@
+import requests
+from datetime import datetime
+import time
+import smtplib
+
+MY_LAT = 29.594
+MY_LONG = 79.302
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "tzid": "Asia/Kolkata",
+    "formatted": 0
+}
+
+def is_night():
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    #THIS API TAKES TWO  INPUTS, Lat, Lng)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+is_night()
+
+
+
+def is_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+is_overhead()
+
+
+MY_EMAIL = "harahitpant@999gmail.com"
+MY_PASSWORD = "bmuqucylstgrfave"
+
+if is_overhead() and is_night():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:ISS Alert!\n\nLook up! ISS is above you!"
+        )
+        print("Email sent!")
+#starttls() → encrypts the connection 🔒
+#Use App Password not your real Gmail password
+#\n\n separates subject from body
+    while True:
+        if  is_overhead() and is_night():
+            #send email
+            print("sending email")
+        time.sleep(60) #checks every 60 second
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day-33/Kanye_nest_quotes.py
@@ -0,0 +1,17 @@
+import requests
+response = requests.get(url="https://api.kanye.rest")
+#print(response)
+
+#response is a requests's method
+#[200] - response codes, like 404 not found error
+#1## : hold on
+#2## : here you go
+#3## : Go away
+#4## : you screwed up
+#5## : i screwed up
+
+response.status_code
+response.raise_for_status()
+
+data = response.json()
+print(data)
